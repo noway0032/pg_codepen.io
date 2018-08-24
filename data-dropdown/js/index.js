27d686da43ce2b79df6744955fea655ccd0f2c1d
noway0032@gmail.com <noway0032@gmail.com>
2018-08-24 11:32:20 +0200

    data-dropdown

---
+++ data-dropdown/js/index.js
@@ -0,0 +1,32 @@
+$(".dropdown div a").on('click', function() {
+  $(".dropdown div ul").slideToggle('fast');
+});
+$(".dropdown div div li a").on('click', function() {
+  $(".dropdown div ul").hide();
+});
+
+function getSelectedValue(id) {
+  return $("#" + id).find("div a span.value").html();
+}
+
+$(document).bind('click', function(e) {
+  var $clicked = $(e.target);
+  if (!$clicked.parents().hasClass("dropdown")) $(".dropdown div ul").hide();
+});
+
+$('.mutliSelect input[type="checkbox"]').on('click', function() {
+
+  var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
+    title = $(this).val() + ",";
+
+  if ($(this).is(':checked')) {
+    var html = '<span title="' + title + '">' + title + '</span>';
+    $('.multiSel').append(html);
+    $(".hida").hide();
+  } else {
+    $('span[title="' + title + '"]').remove();
+    var ret = $(".hida");
+    $('.dropdown dt a').append(ret);
+
+  }
+});
